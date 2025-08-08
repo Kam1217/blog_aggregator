b9cbcf930a1b51f773fbfd1b1aaee2b2ff35e662
Kamila Ostroga <kamilaostroga@gmail.com>
2025-08-08 15:31:59 +0100

    update main.go to run config functions

--- main.go
+++ main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/Kam1217/blog_aggregator/internal/config"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	ptr, err := config.Read()
+	if err != nil {
+		log.Fatal("failed to read the config file: ", err)
+	}
+
+	if err := ptr.SetUser("Kamila"); err != nil {
+		log.Fatal("failed to set user: ", err)
+	}
+
+	updated_conf, err := config.Read()
+	if err != nil {
+		log.Fatal("failed to read the config file: ", err)
+	}
+
+	fmt.Printf("%+v\n", updated_conf)
 }
