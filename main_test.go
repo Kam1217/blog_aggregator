27cf045bb5f44765071e105c15cf2e18f2b9a7cb
Kamila Ostroga <kamilaostroga@gmail.com>
2025-08-12 09:36:40 +0100

    test register command

---
+++ main_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCommandsRegister(t *testing.T) {
+	cmds := commands{
+		registeredCommands: make(map[string]func(*state, command) error),
+	}
+
+	testHandler := func(s *state, cmd command) error {
+		return nil
+	}
+
+	t.Run("Register single command", func(t *testing.T) {
+		cmds.register("test", testHandler)
+		if len(cmds.registeredCommands) != 1 {
+			t.Errorf("Expected 1 registered command but got %d", len(cmds.registeredCommands))
+		}
+
+		if cmds.registeredCommands["test"] == nil {
+			t.Errorf("Expected 'test' command to be registered")
+		}
+	})
+}
