e50e24ffdcd976090987102b92d01e9c50f98baa
Kamila Ostroga <kamilaostroga@gmail.com>
2025-08-12 09:46:40 +0100

    test run command - nonexistent command

--- main_test.go
+++ main_test.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"errors"
 	"testing"
+
+	"github.com/Kam1217/blog_aggregator/internal/config"
 )
 
 func TestCommandsRegister(t *testing.T) {
@@ -28,6 +31,35 @@ func TestCommandsRegister(t *testing.T) {
 		cmds.register("test_2", testHandler)
 		if len(cmds.registeredCommands) != 2 {
 			t.Errorf("Expected 2 registered commands but got %d", len(cmds.registeredCommands))
+		}
+	})
+}
+
+func TestCommandRun(t *testing.T) {
+	cmds := commands{
+		registeredCommands: make(map[string]func(*state, command) error),
+	}
+
+	cfg := &config.Config{DbURL: "test_db", CurrentUserName: ""}
+	cfgMgr := &config.ConfigManager{Path: "/temp/test"}
+	s := &state{cfg: cfg, cfgManager: cfgMgr}
+
+	successHandler := func(s *state, cmd command) error {
+		return nil
+	}
+
+	failedHandler := func(s *state, cmd command) error {
+		return errors.New("failed handler")
+	}
+
+	cmds.register("success", successHandler)
+	cmds.register("fail", failedHandler)
+
+	t.Run("No command", func(t *testing.T) {
+		cmd := command{name: "nonexistent", args: []string{}}
+		err := cmds.run(s, cmd)
+		if err == nil {
+			t.Error("Expected error for non-existent command")
 		}
 	})
 }
