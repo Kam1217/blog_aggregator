2df1765e984e2c212eec38e0f136b1efa4e4f43d
Kamila Ostroga <kamilaostroga@gmail.com>
2025-08-18 13:10:55 +0100

    delete follows query

--- internal/database/feed_follows.sql.go
+++ internal/database/feed_follows.sql.go
@@ -69,6 +69,21 @@ func (q *Queries) CreateFeedFollow(ctx context.Context, arg CreateFeedFollowPara
 	return i, err
 }
 
+const deleteFollows = `-- name: DeleteFollows :exec
+DELETE FROM feed_follows
+WHERE user_id = $1 AND feed_id = $2
+`
+
+type DeleteFollowsParams struct {
+	UserID uuid.UUID
+	FeedID uuid.UUID
+}
+
+func (q *Queries) DeleteFollows(ctx context.Context, arg DeleteFollowsParams) error {
+	_, err := q.db.ExecContext(ctx, deleteFollows, arg.UserID, arg.FeedID)
+	return err
+}
+
 const getFeedFollowsForUser = `-- name: GetFeedFollowsForUser :many
 SELECT 
     feed_follows.id, feed_follows.created_at, feed_follows.updated_at, feed_follows.user_id, feed_follows.feed_id, 
