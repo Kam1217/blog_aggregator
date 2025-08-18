d725bf86753c1660da917788eceafdf2e130d424
Kamila Ostroga <kamilaostroga@gmail.com>
2025-08-18 11:08:51 +0100

    create feed follow query

---
+++ sql/queries/feed_follows.sql
@@ -0,0 +1,16 @@
+--name: CreateFeedFollow :one
+WITH inserted AS (
+  INSERT INTO feed_follows (id, created_at, updated_at, user_id, feed_id) 
+  VALUES (
+    $1,
+    $2,
+    $3,
+    $4,
+    $5
+  ) 
+  RETURNING *
+)
+SELECT inserted.*, users.name AS user_name, feeds.name AS feed_name
+FROM inserted
+JOIN users ON inserted.user_id = users.id
+JOIN feeds ON inserted.feed_id = feeds.id;
